052bafdcc73f910e43a84ad9bd213a7997cfc5ff
Harsh Tripathi <108024082+alpharsh@users.noreply.github.com>
2023-09-10 16:51:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initializing aos
+AOS.init({
+    offset: 250,
+});
+
+var typed = new Typed('#element', {
+    strings: ['Web-Developer', 'Graphic Designer', '<strong>UI/UX</strong> Designer'],
+    typeSpeed: 50,
+    loop: true
+});
+
+// var typed = new Typed('#element', {
+//     strings: ['git push --force ^1000\n `pushed to origin with option force`'],
+// });
