92b6b43f1db41c6e91d66da6cbd94c4c449d84fa
Harsh Tripathi <alpharsh1@gmail.com>
2025-01-30 01:03:37 +0530

    Implement About and Services components with responsive design and styling

--- src/components/Servicess/Services.jsx
+++ src/components/Servicess/Services.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { services } from "../../data";
+import ServiceCard from "../../ui/servicecard/ServiceCard";
 
 function Services() {
   return (
-    <div>Services</div>
-  )
+    <section id="services">
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">Services</h2>
+        </div>
+        <div className="services__group">
+          {services.map(
+            ({ title, description, icon, color, colorRGB }, index) => (
+              <ServiceCard 
+              title={title}
+              description={description}
+              icon={icon}
+              color={color}
+              colorRGB={colorRGB}
+              key={index} />
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
