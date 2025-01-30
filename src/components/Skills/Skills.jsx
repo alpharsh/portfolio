6741ab7677bd6dd2a431ba1190f73ab67cd3a763
Harsh Tripathi <alpharsh1@gmail.com>
2025-01-30 16:19:54 +0530

    Add Skills section with responsive SkillCard components

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './skills.css'
+import { skills } from "../../data";
+import SkillCard from '../../ui/Skillcard/SkillCard';
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <section id="skill">
+      <div className="section__wrapper">
+        <div className="section__header">
+          <h2 className="shine">My Skills</h2>
+        </div>
+        <div className="skill__container">
+          {skills.map((list, index) => (
+            <SkillCard key={index} {...list} /> //from here understand the spread operator
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
